653bfdc84dc0482b9cbc9bfc602bb6b5b6ed8b9b
aalvaroo7 <almarrom@myuax.com>
2023-11-09 12:58:16 +0100

    comit ej 6

--- ej5_estudianteampliado.cpp
+++ ej5_estudianteampliado.cpp
@@ -10,15 +10,31 @@ public:
     Estudiante(string nombre) : nombre(nombre) {}
 
     void registrar_materia(const string& asignaturas) {
-        asdignaturas.push_back(asignaturas);
+        asignaturas.push_back(asignaturas);
     }
 
     void listar_materias() {
         cout << nombre << " ha registrado las siguientes materias: ";
-        for (const string& asignatura : asignatura) {
-            cout << materia << ", ";
+        for (const string& materia : materias) {
+            cout << asignaturas << ", ";
         }
         cout << endl;
     }
+private:
+    string nombre;
+    vector<string> materias;
+};
+
+int main() {
+    Estudiante estudiante("Juan");
+
+    estudiante.registrar_materia("Matematicas");
+    estudiante.registrar_materia("Fisica");
+    estudiante.registrar_materia("Programacion");
+
+    estudiante.listar_materias();
+
+    return 0;
+}
 
 
