067a18edafbefd37245c2750e4d8f10469ca9469
aalvaroo7 <almarrom@myuax.com>
2023-11-09 12:37:43 +0100

    comit ej 3

--- ej4_trycatchthrow.cpp
+++ ej4_trycatchthrow.cpp
@@ -1,3 +1,13 @@
-//
-// Created by ALVARO MARTIN on 09/11/2023.
-//
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+//uso la division de cero como un ejemplo de excepcion
+//uso un tipo de excepcion que ya existe en c++
+//runtime_error
+//runtime_error es una clase que hereda de la clase exception
+//runtime_error es una clase que representa un error en tiempo de ejecucion
+int division(int y,int x){
+    if (x==0)
+        throw runtime_error("Error: division por cero");
+    return y/x;
