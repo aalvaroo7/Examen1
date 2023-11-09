3e1899aea257f46506a772bf8e59235e87ef9758
aalvaroo7 <almarrom@myuax.com>
2023-11-09 12:54:15 +0100

    comit ej 5

--- ej5_estudianteampliado.cpp
+++ ej5_estudianteampliado.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 #include <string>
 #include <vector>
-private:
-string nombre;
-int edad;
-string grado;
+#include <iostream>
+#include <vector>
+#include <string>
+class Estudiante {
+public:
+    Estudiante(string nombre) : nombre(nombre) {}
+
+    void registrar_materia(const string& asignaturas) {
+        asdignaturas.push_back(asignaturas);
+    }
+
+    void listar_materias() {
+        cout << nombre << " ha registrado las siguientes materias: ";
+        for (const string& asignatura : asignatura) {
+            cout << materia << ", ";
+        }
+        cout << endl;
+    }
+
 
