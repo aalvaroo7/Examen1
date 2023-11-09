7baed6e6d7043a30e7cc8228ff0e5ea003af7c29
aalvaroo7 <almarrom@myuax.com>
2023-11-09 12:24:02 +0100

    comit ej 2

--- ej3_gestionmemoria.cpp
+++ ej3_gestionmemoria.cpp
@@ -3,5 +3,7 @@ using namespace std;
 
 //defino una funcion intercambiar que cambia dos punteros a enteros
 void intercambiar(int *a,int *b){
-
+//como no se pueden asignar punteros a punteros, creo una variable temporal
+//llamada mom que almacena el valor de a
+int mom=*a;
 }
