8b1a344c2f9bd176a7acdf2714253cad9b441fea
aalvaroo7 <almarrom@myuax.com>
2023-11-09 13:28:52 +0100

    comit ej 9

--- ej9_gestionexcep.cpp
+++ ej9_gestionexcep.cpp
@@ -12,5 +12,21 @@ private:
     string asignatura;
 };
 class Estudiante {
-
+public:
+    Estudiante(string nombre) : nombre(nombre) {}
+    void registrar_materia(const string& asignatura) {
+        for (const string& asignaturaRegistrada : asignaturas) {
+            if (asignaturaRegistrada == asignatura) {
+                throw asignaturaRegistradaException(asignatura);
+            }
+        }
+        asignaturas.push_back(asignatura);
+    }
+    void listar_materias() {
+        cout << nombre << " ha registrado las siguientes materias: ";
+        for (const string& asignatura : asignaturas) {
+            cout << asignatura << ", ";
+        }
+        cout << endl;
+    }
 };
