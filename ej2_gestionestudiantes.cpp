f620f4d4adb91ab3e4768319cf1a2c53651fb891
aalvaroo7 <almarrom@myuax.com>
2023-11-09 12:03:53 +0100

    comit ej 2

--- ej2_gestionestudiantes.cpp
+++ ej2_gestionestudiantes.cpp
@@ -6,3 +6,10 @@ private:
     string nombre;
     int edad;
     string grado;
+public:
+    // Constructor para inicializar las propiedades del estudiante
+    Estudiante() {
+        nombre = "";
+        edad = 0;
+        grado = "";
+    }
