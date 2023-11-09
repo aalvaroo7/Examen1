f998f02b0e79205a5e3e5cff913bde18d0e0d097
aalvaroo7 <almarrom@myuax.com>
2023-11-09 13:18:17 +0100

    comit ej 8

---
+++ ej8_fitradoestgrado.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <string>
+
+class Estudiante {
+public:
+    Estudiante(string nombre, int grado) : nombre(nombre), grado(grado) {}
+    string obtenerNombre() const {
+        return nombre;
+    }
+    int obtenerGrado() const {
+        return grado;
+    }
+private:
+    string nombre;
+    int grado;
+};
+
+
+
+
