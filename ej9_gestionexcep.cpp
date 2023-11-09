a41b7d96d081d13876f4956d34fce78e6e99f344
aalvaroo7 <almarrom@myuax.com>
2023-11-09 13:40:10 +0100

    comit ej 9

--- ej9_gestionexcep.cpp
+++ ej9_gestionexcep.cpp
@@ -11,3 +11,19 @@ public:
     string getMateria() const {
         return materia;
     }
+private:
+    string materia;
+};
+
+class Alumno {
+public:
+    Alumno(string nombre) : nombre(nombre) {}
+
+    void inscribir_curso(const string& curso) {
+        for (const string& cursoInscrito : cursos) {
+            if (cursoInscrito == curso) {
+                throw MateriaYaRegistradaException(curso);
+            }
+        }
+        cursos.push_back(curso);
+    }
