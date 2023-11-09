d9860cb5377c9c7e29467be8393a0f67ae97a50b
aalvaroo7 <almarrom@myuax.com>
2023-11-09 13:04:21 +0100

    comit ej 6

--- ej6_promedios.cpp
+++ ej6_promedios.cpp
@@ -9,5 +9,14 @@ public:
     void mostrar_asistencia() {
         cout << "Fecha de la clase: " << fecha << ", Estado de asistencia: " << estado << endl;
     }
+private:
+    string fecha;
+    string estado;
+};
+int main(){
+    RegistroAsistencia asistencia("2021-10-01", "Presente");
+    asistencia.mostrar_asistencia();
+    return 0;
+}
 
 
